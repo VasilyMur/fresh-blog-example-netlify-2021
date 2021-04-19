7b1f97ee361d02f560df6be4adf1b1e666a03dbc
vasilymur <vmouravi@gmail.com>
2021-04-19 16:50:48 +0300

    pwa test 2

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/assets/images/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
