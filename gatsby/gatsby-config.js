c6288a034dd43f169af4d57bba63c75c65323f99
vasilymur <vmouravi@gmail.com>
2021-04-19 17:03:22 +0300

    pwa refresh

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -24,19 +24,5 @@ module.exports = {
         token: process.env.SANITY_TOKEN,
       },
     },
-    //
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `src/assets/images/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
   ],
 };
